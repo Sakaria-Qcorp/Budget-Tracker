8c0098acbf13e02ec3463b8d30a6132f6f6055cc
Zack <ahmedsakaria32@gmail.com>
2021-06-24 04:24:32 -0500

    adding the budgetStore

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -5,5 +5,7 @@ const request = window.open("BudgetDB", 1);
 
 request.onupgradeneeded = function( event ) {
    db = event.target.result;
+   //creating the object store
+   const budgetStore = db.createObjectStore("budgetStore", {autoIncrement: true});
 
 };
